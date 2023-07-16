8816c172d41e8a49182e6290b7c1df8115965369
Dilan Madusanka <madmadusanka@gmail.com>
2023-07-16 15:46:42 +0530

    fetch messages against user

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\PropertyEnquire;
 use Flasher\Laravel\Facade\Flasher;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
@@ -13,6 +15,22 @@ class MessageController extends Controller
     public function index()
     {
         $enquires = PropertyEnquire::latest()->paginate(10);
+
+
+        $user = Auth::user();
+        if($user && $user->getAttribute('role') == 'user')
+        {
+            $enquires =DB::table('property_enquires as pe')
+            ->leftJoin('properties as p', 'p.id', '=', 'pe.propertyId')
+            ->where('p.user_id', '=', $user->id)
+            ->orderBy('pe.created_at', 'desc')
+            ->paginate(20);
+        }
+        else{
+            $enquires = PropertyEnquire::latest()->paginate(10);
+        }
+
+
         return view('dashboard.messages.index')->with('enquires',$enquires);
     }
 
